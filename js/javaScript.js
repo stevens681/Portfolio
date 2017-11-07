6c00671c9c1bd6c5f1cd7a80e6ab91707908ac7b
Fernando Rosa <stevensfrr@gmail.com>
2017-11-07 04:37:59 +0000

    first commit

---
+++ js/javaScript.js
@@ -0,0 +1,55 @@
+
+// variables
+var $header_top = $('.header-top');
+var $nav = $('nav');
+
+
+
+// fullpage customization
+$('#fullpage').fullpage({
+//  sectionsColor: ['#5b7871', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+  sectionSelector: '.vertical-scrolling',
+  slideSelector: '.horizontal-scrolling',
+  navigation: true,
+  slidesNavigation: true,
+  controlArrows: false,
+  anchors: ['firstSection', 'secondSection', 'thirdSection', 'fourthSection', 'fifthSection'],
+
+
+  afterLoad: function(anchorLink, index) {
+    $header_top.css('background', 'rgba(0, 47, 77, .3)');
+    $nav.css('background', 'rgba(0, 47, 77, .25)');
+    if (index == 5) {
+        $('#fp-nav').hide();
+      }
+  },
+
+  onLeave: function(index, nextIndex, direction) {
+    if(index == 5) {
+      $('#fp-nav').show();
+    }
+  },
+
+  onSlideLeave: function( anchorLink, index, slideIndex, direction) {
+    if(anchorLink == 'fifthSection' && slideIndex == 1) {
+      $.fn.fullpage.setAllowScrolling(true, 'up');
+      $header_top.css('background', 'rgba(0, 47, 77, .3)');
+      $nav.css('background', 'rgba(0, 47, 77, .25)');
+    }
+  } 
+})
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+//$(window).scroll(function () {
+//    if ($(window).scrollTop() > 100) {
+//        $('.topnav').css('top', $(window).scrollTop());
+//    }
+//}
+//);
